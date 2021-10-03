d55b08b9e1a5219ddac722a28ea088e6b62e3cfe
chilubagh <ibrahim.dauda@integrify.io>
2021-10-04 00:20:55 +0300

    vue amplify libraries were imported

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import amplify, * as AmplifyModules from "@aws-amplify";
+import { AmplifyPlugin } from "@aws-amplify/vue";
+import awsconfig from "./aws-exports";
 
-Vue.config.productionTip = false
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap - vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+amplify.configure(awsconfig);
+Vue.use(AmplifyPlugin, AmplifyModules);
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
